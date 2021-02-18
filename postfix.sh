5bf2f8452e0e6da6630a142c55c1ffb64c897533
JKSTAFF <ionsuite@gmail.com>
2021-02-18 14:47:38 +0800

    customize parameters

---
+++ postfix.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright (c) 2012-2020 Vector Di-gi <https://vectordigi.cf>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/Vector-Digi/match_complier
+# File name: postfix.sh
+# Description: OpenWrt customize script phase 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Change default locale to Greater China
+sed -i 's/auto/zh_cn/g' feeds/luci/modules/luci-base/root/etc/config/luci && sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate && sed -i '/CST-8/a\set system.@system[-1].zonename='"'"'Asia/Shanghai'"'"'' package/base-files/files/bin/config_generate
+
+# Set default gateway to '10.0.0.1'
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate && sed -i 's/10.0.1/10.0.0/g' package/base-files/files/bin/config_generate
+
+# Activate wireless at reset
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Set 'toor' as the default password for root user
+sed -i 's*root::0:0:99999:7:::*root:\$1\$Z5PSAHJ9$1UReP9Mm94CqDFVEnROB//:17713:0:99999:7:::*g' package/
+
+# Add  3rd-part packages
+git clone -b packages https://github.com/Vector-Digi/match_complier.git package/custom
+
+# Enable MIPS FPU emulator
+if grep -q 'CONFIG_TARGET_ramips=y' .config  &&  ! grep -q 'CONFIG_KERNEL_MIPS_FP_SUPPORT=y' .config ;
+then 
+    echo "CONFIG_KERNEL_MIPS_FP_SUPPORT=y" >> .config && echo "FPU emulator enabled. Do not modify menuconfig again"
+else echo "Not MIPS architecture or parameter already exist. Skip"
+fi
--- prefix.sh
+++ prefix.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2012-2020 Vector Di-gi <https://vectordigi.cf>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# https://github.com/Vector-Digi/match_complier
+# File name: prefix.sh
+# Description: OpenWrt customize script phase 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
